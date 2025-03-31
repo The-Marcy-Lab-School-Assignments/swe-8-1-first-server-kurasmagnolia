2b22f99e0212936384fb82087abfd756709251d3
kurasmagnolia <kingmmcleod@outlook.com>
2025-03-31 12:41:29 -0400

    declared PORT + app listening to that PORT

--- server/index.js
+++ server/index.js
@@ -5,3 +5,11 @@ const express = require('express');
 
 // invoking express to create our app
 const app = express();
+
+// declared PORT i'll be using
+const PORT = 8080;
+
+// invoked .listen on app to listen to my PORT i declared
+app.listen(PORT, () => {
+  console.log(`listening at this PORT: ${PORT}`);
+});
